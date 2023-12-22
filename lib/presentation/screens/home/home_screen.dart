1fe387568e8a569611181091e67fbe43e11311f4
Felipe Torres <ctorres@s10peru.com>
2023-12-22 18:10:24 -0500

    Buttons finished

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widgets_app/config/menu/menu_item.dart';
+//import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+//import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 
 class HomeScreen extends StatelessWidget {
+  
+  static const String name = 'home_screen';
   const HomeScreen({super.key});
 
   @override
@@ -16,9 +21,9 @@ class HomeScreen extends StatelessWidget {
 }
 
 class _HomeView extends StatelessWidget {
-  const _HomeView({
+  /*const _HomeView({
     super.key,
-  });
+  });*/
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +31,6 @@ class _HomeView extends StatelessWidget {
     return ListView.builder(
       itemCount: appMenuItems.length,
       itemBuilder: (context, index) {
-
         final menuItem = appMenuItems[index];
         return _CustomListTile(menuItem: menuItem);
         //return Text(menuItem.title);
@@ -44,15 +48,23 @@ class _CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final colors = Theme.of(context).colorScheme;
 
     return ListTile(
-      leading: Icon( menuItem.icon, color: colors.primary ),
-      trailing: Icon( Icons.arrow_forward_ios_rounded, color: colors.primary),
+      leading: Icon(menuItem.icon, color: colors.primary),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
       title: Text(menuItem.title),
       subtitle: Text(menuItem.subTitle),
-      onTap: (){},
-      );
+      onTap: () {
+       /* Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const ButtonsScreen(),
+          ),
+        );*/
+        //Navigator.pushNamed(context, menuItem.link);
+        context.push(menuItem.link);
+        //context.pushNamed( CardsScreen.name );
+      },
+    );
   }
 }
